726cc18005aa11239267f7a24dd1df2755d1eb70
Your Name <you@example.com>
2023-11-22 15:26:16 +0700

    add new file

--- airflow-dags/dags/dag_k8s_spark.py
+++ airflow-dags/dags/dag_k8s_spark.py
@@ -1,44 +1,53 @@
+from __future__ import annotations
+
+import os
 from datetime import datetime, timedelta
-
 from airflow import DAG
-from airflow.configuration import conf
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from kubernetes.client import models as k8s
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
-default_args = {
-    'owner': 'ndquoc',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 11, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
+# [END import_module]
 
-namespace = conf.get("kubernetes", "NAMESPACE")
 
-if namespace == 'default':
-    config_file = "C:/Users/nduyq/pyspark/code.yaml"
-    in_cluster = False
-else:
-    in_cluster = True
-    config_file = None
+# [START instantiate_dag]
 
-dag = DAG(
-    dag_id='dag-test',
-    default_args=default_args,
-    schedule_interval='@once',
-)
-task1 = KubernetesPodOperator(
-    namespace=namespace,
-    image="ndquoc1306/spark:0.1.0",
-    name="task-test",
-    task_id="cf0001-1",
-    in_cluster=in_cluster,
-    cluster_context='docker-desktop',
-    config_file=config_file,
-    is_delete_operator_pod=True,
-    get_logs=True
-)
 
-task1
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+DAG_ID = "spark_pi"
+
+with DAG(
+    DAG_ID,
+    default_args={"max_active_runs": 1},
+    description="submit spark-pi as sparkApplication on kubernetes",
+    schedule=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+) as dag:
+    # [START SparkKubernetesOperator_DAG]
+    t1 = SparkKubernetesOperator(
+        task_id="spark_pi_submit",
+        namespace="default",
+        application_file="config_spark.yaml",
+        do_xcom_push=True,
+        dag=dag,
+    )
+
+    t2 = SparkKubernetesSensor(
+        task_id="spark_pi_monitor",
+        namespace="default",
+        application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
+        dag=dag,
+    )
+    t1 >> t2
+
+    # [END SparkKubernetesOperator_DAG]
+    from tests.system.utils.watcher import watcher
+
+    # This test needs watcher in order to properly mark success/failure
+    # when "tearDown" task with trigger rule is part of the DAG
+    list(dag.tasks) >> watcher()
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
